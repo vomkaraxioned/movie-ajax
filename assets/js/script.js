c5a32de69fdceff82a06d8aaf102d150bd37e222
vomkaraxioned <omkarv@axioned.com>
2022-09-06 19:31:25 +0530

    js added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,76 @@
 /* Author: 
 
 */
+//Name, release-date, actor name, Producer Name 
+let movie = {
+    data: [{
+            name: "Brahmastra",
+            releaseDate: "09/09/2022",
+            actorName: "Ranbir Kapoor",
+            producerName: "Karan Johar"
+        },
+        {
+            name: "Cutputlli",
+            releaseDate: "02/09/2022",
+            actorName: "akii",
+            producerName: "ratsasan"
+        },
+        {
+            name: "Salaar",
+            releaseDate: "09/09/2022",
+            actorName: "prabhas",
+            producerName: "hombale"
+        }, {
+            name: "avatar 2",
+            releaseDate: "09/09/2022",
+            actorName: "alien",
+            producerName: "jaadu"
+        }, {
+            name: "Adipurush",
+            releaseDate: "12/01/2023",
+            actorName: "prabhas",
+            producerName: "om raut"
+        }, {
+            name: "karthikeya 2",
+            releaseDate: "09/09/2022",
+            actorName: "siddhart",
+            producerName: "zee"
+        }, {
+            name: "fighter",
+            releaseDate: "09/09/2022",
+            actorName: "hritik roshan",
+            producerName: "papa roshan"
+        }, {
+            name: "Ni po",
+            releaseDate: "09/11/2022",
+            actorName: "salmon",
+            producerName: "Azgar"
+        }, {
+            name: "Ultra man",
+            releaseDate: "09/09/2022",
+            actorName: "lamie o",
+            producerName: "angie pillai"
+        }, {
+            name: "ninja technique",
+            releaseDate: "05/011/2022",
+            actorName: "hatoori",
+            producerName: "akie takie"
+        }
+    ],
+    length: 10
+};
 
+const search = document.querySelector("form");
 
+search.addEventListener('submit', showData);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function showData() {
+    let key = document.forms[0]["search-key"].value;
+    let list = document.querySelector(".data");
+    let data = movie.data;
+    for (x in data) {
+        if (key == data[x].name || key == data[x].releaseDate || key == data[x].actorName || key == data[x].producerName) {
+            list.innerHTML += "<li class=\"movie\"><h2>" + data[x].name + "<h2><p class=\"movie-info\">" + data[x].releaseDate + "</p></li>";
+        }
+    }
+}
